344c8ad54553d6c75841d7dcda8725556bc2896c
Julio Cesar Monroy Casimiro <julio.monroy@tucasaexpress.com>
2025-02-16 23:45:38 -0600

    Quitar modulos y rutas para cargar componentes standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,33 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'products',
-        loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+        path: 'products/product',
+        loadComponent: () => import('./products/pages/product-page/product-page.component').then(c => c.ProductPageComponent)
     },
     {
         path: 'signals',
-        loadChildren: () => import('./signals/signals.module').then(m => m.SignalsModule)
+        loadComponent: () => import('./signals/layout/signals-layout.component').then(c => c.SignalsLayoutComponent),
+        children: [
+          {
+            path: 'counter',
+            loadComponent: () => import('./signals/pages/counter-page/counter-page.component').then(c => c.CounterPageComponent)
+          },
+          {
+            path: 'user-info',
+            loadComponent: () => import('./signals/pages/user-info-page/user-info-page.component').then(c => c.UserInfoPageComponent)
+          },
+          {
+            path: 'properties',
+            loadComponent: () => import('./signals/pages/properties-page/properties-page.component').then(c => c.PropertiesPageComponent)
+          },
+          {
+            path: '**',
+            redirectTo: 'counter'
+          },
+        ]
     },
     {
         path: '**',
-        redirectTo: 'products'
+        redirectTo: 'products/product'
     }
 ];
